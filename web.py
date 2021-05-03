70de26081de76b67967244a635fef97b989aeb6c
yusef320 <82632877+yusef320@users.noreply.github.com>
2021-05-03 07:15:10 +0200

    Update web.py

--- web.py
+++ web.py
@@ -168,12 +168,12 @@ for p in range(0,15):
     df_verano = (d.groupby("País origen")["Precio"].mean()/d2.groupby("País origen")["Precio"].mean()-1)*100
     d = d.loc[d["Es directo"]==1]
     d2 = d2.loc[d2["Es directo"]==1]
-    df_demanda = (d.groupby("Ciudad de destino")["Es directo"].sum()/d2.groupby("Ciudad de destino")["Es directo"].sum()-1)*100
+    df_demanda = (d.groupby("País origen")["Es directo"].sum()/d2.groupby("País origen")["Es directo"].sum()-1)*100
     s= round(df_verano[mercado],2)
     fecha = f"{dia.month:02d}-{dia.day:02d}"
     datos.append(s)
     fec.append(fecha)
-    o= round(df_demanda[provincia],2)
+    o= round(df_demanda[mercado],2)
     oferta.append(o)
 st.subheader(f"Variación de la demanda de {mercado}.")
 st.markdown(f"Muestra el comportamiento del mercado para los vuelos procedentes de {mercado}.")
