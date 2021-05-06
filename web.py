785b56930e3a1d6506abcdc55422dd7e8b5f2106
yusef320 <82632877+yusef320@users.noreply.github.com>
2021-05-06 08:19:09 +0200

    Update web.py

--- web.py
+++ web.py
@@ -55,9 +55,9 @@ def variacion(provincia,delta, mercado, rang, x,i):
         mercado = provincia
     else:
         a = "País origen"
-
-    for z in range(0,3):
-        dia = datetime.datetime.now() + datetime.timedelta(days=z+i)
+        
+    for z in range(1,4):
+        dia = datetime.datetime.now() + datetime.timedelta(days=z-i)
         fecha = f"{dia.month:02d}-{dia.day:02d}"
         fec.append(fecha)
 
