eb92d8d50d8949404a9fc9153447f0ac8598c080
yusef320 <82632877+yusef320@users.noreply.github.com>
2021-05-13 00:45:23 +0200

    Update web.py

--- web.py
+++ web.py
@@ -193,7 +193,7 @@ st.sidebar.markdown(f"""
 #### Newsletter
 Recibe un email una vez a la semana con la información relevante para {provincia}.
 """)
-col1,col2 = st.sidebarbeta_columns([2,1]) 
+col1,col2 = st.sidebar.beta_columns([2,1]) 
 
 email = col1.text_input(f'Suscríbete a nuestro newsletter sobre {provincia}', 'ejemplo@mail.com')
 a = col2.button("Suscribir")
