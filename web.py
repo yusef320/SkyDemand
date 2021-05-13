6f25821ed2c1c7bb20b0d5b915eebc6fd2d23eeb
yusef320 <82632877+yusef320@users.noreply.github.com>
2021-05-13 20:48:56 +0200

    Update web.py

--- web.py
+++ web.py
@@ -230,7 +230,7 @@ if a:
 #Parte central
 
 image = Image.open('logo.png')
-st.image(image, width=800)
+st.image(image, width=400)
 f"""
 Descubre cómo cambia la oferta de vuelos hacia tu ciudad y adelanta tu negocio al mercado.
 """
