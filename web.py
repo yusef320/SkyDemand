4b8e4e40a109fb404b14041804ef8e46845f8a70
yusef320 <82632877+yusef320@users.noreply.github.com>
2021-06-17 01:33:09 +0200

    Update web.py

--- web.py
+++ web.py
@@ -28,8 +28,8 @@ def predicción(fec, datos, oferta, dato):
     train = p.drop([dato], axis=1)
     test = p[dato]
     n=0
-    while n < 0.6:
-        s = random.randint(50,100)*0.01
+    while n < 0.9:
+        s = random.randint(60,95)*0.01
         X_train, X_test, Y_train, Y_test = train_test_split(train, test, test_size=s, random_state=1)
         regr = LinearRegression()
         regr.fit(X_train, Y_train)
